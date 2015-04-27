9a9e6d50972e7f61d80132c003483b70978f4c32
okkey <iam@okkeysumiyoshi.com>
2015-04-28 09:37:11 +1000

    Initial commit

---
+++ ang_app.js
@@ -0,0 +1,42 @@
+
+(function () {
+
+    var myApp = angular.module('d3_app', []);
+
+
+    myApp.controller('CommandCtrl', function ($scope) {
+
+        
+
+        $scope.create = function (_d) {
+			console.log("angular called");
+			console.log(_d);
+        }
+
+
+        this.place = function () {
+
+
+        }
+
+        this.forward = function () {
+
+        }
+
+        this.turn = function () {
+ 
+        }
+
+        this.clear = function () {
+
+        }
+
+        this.changeRbtName = function () {
+
+        }
+
+
+    }
+)
+})();
+
